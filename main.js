5cb517fdbcea7c3921756eb7086ff5ccad7fa851
Le Nhat Tung <hoanghuuhanh321@gmail.com>
2023-09-06 00:37:16 +0700

    Up Lan thu 3 Da Thay Doi Mau icon Khi Click

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 
-function Value() {
-    var a = document.querySelectorAll(".btn");
-    for (let i = 0; i < a.length; i++) {
-        a[i].addEventListener('click', function () {
+
+
+var a = document.querySelectorAll(".btn");
+for (let i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function () {
+        // Thêm lớp haha cho nút được nhấp
+        this.classList.add("active");
+        if (a[i].className == "btn active") {
             a[i].style.setProperty("color", "#3a88f5");
             a[i].style.borderBottom = "medium solid #3a88f5";
-        })
-    }
-}
+        }
+        // Tìm nút khác có lớp active và xóa lớp đó
 
-
-function Color_Icon_Botton() {
-    Value();
-
+        var b = document.querySelector(".btn.active:not(:nth-child(" + (i + 1) + "))");
+        if (b) {
+            b.classList.remove("active");
+            b.style.removeProperty("color");
+            b.style.removeProperty("border-bottom");
+        }
+    })
 }
 
