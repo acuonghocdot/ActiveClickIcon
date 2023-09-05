911b974cb217f1b2839db78f99e36fcf434bd087
Le Nhat Tung <hoanghuuhanh321@gmail.com>
2023-09-05 16:55:04 +0700

    Up Lan 2

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 
 
+var a = document.querySelectorAll(".btn");
+for (let i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function () {
+        a[i].style.setProperty("color", "#3a88f5");
 
-function Color_Icon_Botton() {
-    var x = document.getElementById("Header_Center_Icon");
-    var y = document.querySelectorAll("div i")
-
-
+        a[i].style.borderBottom = "medium solid #0000FF";
+    })
 
 }
+
+function Color_Icon_Botton() {
+
+}
+
