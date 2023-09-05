66f3e8ad6684924b1f4346b96f2dcab64a6a1e9a
Le Nhat Tung <hoanghuuhanh321@gmail.com>
2023-09-05 18:00:54 +0700

    SUA cHUA xoa color nav heard

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 
-
-var a = document.querySelectorAll(".btn");
-for (let i = 0; i < a.length; i++) {
-    a[i].addEventListener('click', function () {
-        a[i].style.setProperty("color", "#3a88f5");
-
-        a[i].style.borderBottom = "medium solid #0000FF";
-    })
-
+function Value() {
+    var a = document.querySelectorAll(".btn");
+    for (let i = 0; i < a.length; i++) {
+        a[i].addEventListener('click', function () {
+            a[i].style.setProperty("color", "#3a88f5");
+            a[i].style.borderBottom = "medium solid #3a88f5";
+        })
+    }
 }
 
+
 function Color_Icon_Botton() {
+    Value();
 
 }
 
